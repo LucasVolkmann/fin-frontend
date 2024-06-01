f0830fa0aaf2c4449216469b4e47ea52509ea20d
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-06-01 13:58:05 -0300

    fix: fix select history length style

---
+++ src/modules/main/components/sections/history/components/select-history-length/SelectHistoryLength.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
--- src/modules/main/components/sections/history/components/select-history-length/SelectHistoryLength.tsx
+++ src/modules/main/components/sections/history/components/select-history-length/SelectHistoryLength.tsx
@@ -1,13 +1,14 @@
 import { HistoryLengthEnum } from './HistoryLengthEnum';
+import { Select } from './SelectHistoryLength.styles';
 
 const SelectHistoryLength = ({...props}) => {
 
   return (
-    <select {...props}>
+    <Select {...props}>
       <option value={HistoryLengthEnum.THREE_MONTHS} >Últimos três meses</option>
       <option value={HistoryLengthEnum.SIX_MONTHS} >Últimos seis meses</option>
       <option value={HistoryLengthEnum.ONE_YEAR} >Este ano</option>
-    </select>
+    </Select>
   );
 };
 
